df7d04dee27fba5a7d1153afbf1fb6d2818e6a91
ABCDEFJHIJKLMNOPQRSTUVWXYZ <41023362+ABCDEFJHIJKLMNOPQRSTUVWXYZ@users.noreply.github.com>
2018-07-13 11:07:57 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,120 @@
+particlesJS('particles-js',
+
+  {
+    "particles": {
+      "number": {
+        "value": 600,
+        "density": {
+          "enable": true,
+          "value_area": 10000
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "star",
+        "stroke": {
+          "width": 14,
+          "color": "#00e1ff"
+        },
+        "polygon": {
+          "nb_sides": 12
+        },
+        "image": {
+          "src": "http://cdn.gamer-network.net/2018/metabomb/fortniteroadtripanddriftskinsguidesecretbattlestarlocationsandallchallengeslist.jpg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 64.1,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 33.7,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 0,
+        "color": "#ffffff",
+        "opacity": 1,
+        "width": 20
+      },
+      "move": {
+        "enable": true,
+        "speed": 78.6,
+        "direction": "top left",
+        "random": false,
+        "straight": false,
+        "out_mode": "bounce",
+        "attract": {
+          "enable": false,
+          "rotateX": 10000,
+          "rotateY": 10000
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "bubble"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
